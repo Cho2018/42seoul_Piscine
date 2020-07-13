504c2f7f1e24a492c894dbf1f40a1e90b1b7f435
Heejune Cho <hecho@c4r8s8.42seoul.kr>
2020-07-13 18:45:00 +0900

    retry

--- Piscine/C02/ex01/ft_strncpy.c
+++ Piscine/C02/ex01/ft_strncpy.c
@@ -6,30 +6,34 @@
 /*   By: hecho <hecho@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 13:51:20 by hecho             #+#    #+#             */
-/*   Updated: 2020/07/13 13:53:30 by hecho            ###   ########.fr       */
+/*   Updated: 2020/07/13 18:43:37 by hecho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
-	int				check;
 
 	i = 0;
-	check = 0;
-	while (i < n)
+	while (src[i] && i < n)
 	{
-		if (src[i] == '\0' || check == 1)
-		{
-			dest[i] = '\0';
-			check = 1;
-		}
-		else
-			dest[i] = src[i];
+		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	while (i < n)
+		dest[i++] = '\0';
 	return (dest);
+}
+
+int main()
+{
+	char dest[100];
+	char *src;
+
+	src = "hello bye";
+	ft_strncpy(dest, src, 3);
+	printf("%s\n", dest);
 }
