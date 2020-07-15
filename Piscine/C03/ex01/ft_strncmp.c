376e8dfbfa8eb3ce2361424cdc46334ed30ffac4
Heejune Cho <hecho@c4r10s2.42seoul.kr>
2020-07-15 16:25:54 +0900

    retry

--- Piscine/C03/ex01/ft_strncmp.c
+++ Piscine/C03/ex01/ft_strncmp.c
@@ -6,34 +6,27 @@
 /*   By: hecho <hecho@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/15 11:16:47 by hecho             #+#    #+#             */
-/*   Updated: 2020/07/15 11:19:55 by hecho            ###   ########.fr       */
+/*   Updated: 2020/07/15 13:59:35 by hecho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
-#include <string.h>
 
 int		ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < n - 1)
+	while (s1[i] && s2[i] && i < n)
 	{
 		if (s1[i] != s2[i])
-			return s1[i] - s2[i];
+			return (s1[i] - s2[i]);
 		i++;
 	}
-	return s1[i] - s2[i];
-}
-
-int main()
-{
-	char *s1;
-	char *s2;
-
-	s1 = "BlockDmask";
-	s2 = "BlockFmask";
-	printf("%d\n%d\n", ft_strncmp(s1, s2, 6), strncmp(s1, s2, 6));
+	if (i == n)
+	{
+		s1--;
+		s2--;
+	}
+	return (s1[i] - s2[i]);
 }
--- Piscine/C03/ex05/ft_strlcat.c
+++ Piscine/C03/ex05/ft_strlcat.c
@@ -6,48 +6,35 @@
 /*   By: hecho <hecho@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/15 11:24:43 by hecho             #+#    #+#             */
-/*   Updated: 2020/07/15 11:26:26 by hecho            ###   ########.fr       */
+/*   Updated: 2020/07/15 15:27:21 by hecho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
-#include <string.h>
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int i;
-	unsigned int dest_size;
-	unsigned int src_size;
 
-	dest_size = 0;
-	while (dest[dest_size] != '\0')
-		dest_size++;
-	src_size = 0;
-	while (src[src_size] != '\0')
-		src_size++;
-	if (size < dest_size)
-		return (src_size + size);
 	i = 0;
-	dest--;
-	while (*src && i < ((long long)size - dest_size - 1))
+	while (*dest && i < size)
+	{
+		dest++;
+		i++;
+	}
+	while (*src && i + 1 < size)
 	{
 		*dest = *src;
-		i++;
 		dest++;
 		src++;
+		i++;
 	}
-	*dest = '\0';
-	return (src_size + dest_size);
-}
-
-int main()
-{
-	char dest1[20] = "abc";
-	char dest2[20] = "abc";
-	char src[] = "123";
-	unsigned int size = 2;
-
-	printf("%u\n%s\n%lu\n%s\n", ft_strlcat(dest1, src, size), dest1, 
-			strlcat(dest2, src, size), dest2);
+	if (i < size)
+		*dest = 0;
+	while (*src)
+	{
+		src++;
+		i++;
+	}
+	return (i);
 }
