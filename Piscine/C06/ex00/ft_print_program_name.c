8ab98ab94c2ebba6faa607c45433650f1009138b
Heejune Cho <hecho@c4r10s2.42seoul.kr>
2020-07-18 12:05:17 +0900

    modify

--- Piscine/C06/ex00/ft_print_program_name.c
+++ Piscine/C06/ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: hecho <hecho@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/15 17:54:04 by hecho             #+#    #+#             */
-/*   Updated: 2020/07/18 11:28:04 by hecho            ###   ########.fr       */
+/*   Updated: 2020/07/18 12:04:21 by hecho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	ft_putstr(char *str)
 
 int		main(int ac, char *av[])
 {
+	ac = 0;
 	ft_putstr(av[0]);
 	write(1, "\n", 1);
 }
