3aec4869d4de82379f87c6d5dc2a83765eda6643
Heejune Cho <hecho@c4r9s8.42seoul.kr>
2020-07-20 11:01:27 +0900

    C07ex02

---
+++ Piscine/C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hecho <hecho@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/19 12:50:19 by hecho             #+#    #+#             */
+/*   Updated: 2020/07/20 10:59:51 by hecho            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int		ft_ultimate_range(int **range, int min, int max)
+{
+	int i;
+
+	if (min >= max)
+	{
+		range = 0;
+		return (0);
+	}
+	*range = (int *)malloc(sizeof(int) * (max - min));
+	if (!*range)
+		return (-1);
+	i = 0;
+	while (min + i < max)
+	{
+		(*range)[i] = min + i;
+		i++;
+	}
+	return (max - min);
+}
+
+int main()
+{
+	int min = 2;
+	int max = 10;
+	int *answer;
+	int range = ft_ultimate_range(&answer, min, max);
+	printf("%d\n", range);
+
+	for (int i = 0; i < max - min; i++)
+		printf("%d\n", answer[i]);
+}
