e68e96ae8fd450720f097c465694cedb29935f4e
Heejune Cho <hecho@c4r10s2.42seoul.kr>
2020-07-22 17:11:48 +0900

    retry

--- Piscine/C07/ex03/ft_strjoin.c
+++ Piscine/C07/ex03/ft_strjoin.c
@@ -5,16 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hecho <hecho@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/19 13:21:03 by hecho             #+#    #+#             */
-/*   Updated: 2020/07/20 12:46:05 by hecho            ###   ########.fr       */
+/*   Created: 2020/07/21 16:37:49 by hecho             #+#    #+#             */
+/*   Updated: 2020/07/22 16:44:57 by hecho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 
-int		ft_strlen(char *str)
+int			ft_strlen(char *str)
 {
 	int i;
 
@@ -24,59 +22,38 @@ int		ft_strlen(char *str)
 	return (i);
 }
 
-char	*ft_strcat(char *dest, char *src)
+char		*ft_strcpy(char *dest, char *src)
 {
 	while (*src)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
+		*(dest++) = *(src++);
 	return (dest);
 }
 
-char	*ft_strjoin(int size, char **strs, char *sep)
+char		*ft_strjoin(int size, char **strs, char *sep)
 {
-	int		i;
-	int		len;
-	char	*temp;
-	char	*answer;
+	int			i;
+	int			len;
+	char		*temp;
+	char		*answer;
 
-	if (size == 0)
+	if (!size)
 	{
 		answer = (char *)malloc(sizeof(char) * 1);
 		answer[0] = 0;
 		return (answer);
 	}
-	len = ft_strlen(sep) * (size - 1);
+	len = 0;
 	i = 0;
 	while (i < size)
 		len += ft_strlen(strs[i++]);
-	answer = (char *)malloc(sizeof(char) * (len + 1));
-	temp = ft_strcat(answer, strs[0]);
+	answer = (char *)malloc(len + (size - 1) * ft_strlen(sep) + 1);
+	temp = ft_strcpy(answer, strs[0]);
 	i = 1;
 	while (i < size)
 	{
-		temp = ft_strcat(temp, sep);
-		temp = ft_strcat(temp, strs[i++]);
+		temp = ft_strcpy(temp, sep);
+		temp = ft_strcpy(temp, strs[i++]);
 	}
-	temp[i] = '\0';
+	*temp = 0;
 	return (answer);
-}
-
-int main()
-{
-	char *strs[7] = {
-		"abc",
-		"123",
-		"def",
-		"456",
-		"ghi",
-		"789",
-		"xyz"
-	};
-	char sep[] = "+";
-	char *answer = ft_strjoin(3, strs, sep);
-
-	printf("%s\n", answer);
 }
---
+++ Piscine/C07/ex05/ft_split.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hecho <hecho@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/20 18:29:56 by hecho             #+#    #+#             */
+/*   Updated: 2020/07/22 10:08:37 by hecho            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int			is_contain(char c, char *charset)
+{
+	while (*charset)
+	{
+		if (c == *charset)
+			return (1);
+		charset++;
+	}
+	return (0);
+}
+
+long long	word_count(char *str, char *charset)
+{
+	long long count;
+
+	count = 0;
+	while (*str)
+	{
+		if (!is_contain(*str, charset))
+		{
+			count++;
+			while (*str && !is_contain(*str, charset))
+				str++;
+		}
+		str++;
+	}
+	return (count);
+}
+
+void		ft_strcpy(char *dest, char *start, char *end)
+{
+	while (start < end)
+	{
+		*dest = *start;
+		dest++;
+		start++;
+	}
+	*dest = 0;
+}
+
+char		**ft_split(char *str, char *charset)
+{
+	long long	i;
+	char		*start;
+	char		**answer;
+
+	answer = (char **)malloc(sizeof(char *) * word_count(str, charset) + 1);
+	i = 0;
+	while (*str)
+	{
+		if (!is_contain(*str, charset))
+		{
+			start = str;
+			while (*str && !is_contain(*str, charset))
+				str++;
+			answer[i] = (char *)malloc(str - start + 1);
+			ft_strcpy(answer[i++], start, str);
+		}
+		str++;
+	}
+	answer[i] = 0;
+	return (answer);
+}
