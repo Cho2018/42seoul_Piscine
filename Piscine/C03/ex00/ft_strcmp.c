275cb2e5e7bd3eed2d1a298a69fb456974ef972e
Cho2018 <44170716+Cho2018@users.noreply.github.com>
2020-07-26 01:09:17 +0900

    Update ft_strcmp.c

--- Piscine/C03/ex00/ft_strcmp.c
+++ Piscine/C03/ex00/ft_strcmp.c
@@ -22,10 +22,10 @@ int		ft_strcmp(char *s1, char *s2)
 	while (s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
-			return s1[i] - s2[i];
+			return (s1[i] - s2[i]);
 		i++;
 	}
-	return s1[i] - s2[i];
+	return (s1[i] - s2[i]);
 }
 
 int main()
